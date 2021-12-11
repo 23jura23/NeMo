0d9a8698e4296020d37460a6020670bf48230695
Худяков Юрий <catchitoo@mail.ru>
2021-12-12 04:15:38 +0500

    Change hifigan test

--- examples/tts/test/hifigan_test.py
+++ examples/tts/test/hifigan_test.py
@@ -4,11 +4,13 @@ from nemo.collections.tts.models import HifiGanModel
 from nemo.core.config import hydra_runner
 from nemo.utils.exp_manager import exp_manager
 
-@hydra_runner(config_path="conf/hifigan", config_name="hifigan")
+@hydra_runner(config_path="../conf/test", config_name="hifigan_test")
 def main(cfg):
     trainer = pl.Trainer(**cfg.trainer)
     exp_manager(trainer, cfg.get("exp_manager", None))
-    model = HifiGanModel(cfg=cfg.model, trainer=trainer)
+    model = HifiGanModel.restore_from(cfg.checkpoint).eval()
+    model.setup_validation_data(cfg.val_ds)
+    model.set_trainer(trainer)
     trainer.validate(model)
 
 if __name__ == '__main__':
